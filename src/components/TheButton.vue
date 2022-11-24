a65822ab0488677884b2f1d36198175b1e2a0fe8
jocelyn <jocelyn@ecloudmobile.com>
2022-11-24 18:39:02 +0800

    feat(button): 移除空心樣式

--- src/components/TheButton.vue
+++ src/components/TheButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button :class="type" :disabled="disabled">
-    <span class="inner" :class="{ plain: plain }">
+    <span class="inner">
       <slot></slot>
     </span>
   </button>
@@ -10,12 +10,9 @@
 export default {
   props: {
     type: {
+      // 用於區分按鈕用途，可選 'next', 'popup'
       type: String,
       default: 'next'
-    },
-    plain: {
-      type: Boolean,
-      default: false
     },
     disabled: {
       type: Boolean,
@@ -42,27 +39,17 @@ button {
   &:hover {
     .inner {
       background-color: $yellow-light;
-      &.plain {
-        background-color: $gray;
-      }
     }
   }
   &:disabled {
     cursor: not-allowed;
     .inner {
       background-color: $yellow-dark;
-      &.plain {
-        background-color: $gray-dark;
-      }
     }
     &:active {
       .inner {
         background-color: $yellow-dark;
         transform: translate3d(-6px, -6px, 0);
-        &.plain {
-          background-color: $gray-dark;
-          transform: translate3d(-6px, -6px, 0);
-        }
       }
     }
   }
@@ -87,8 +74,5 @@ button {
   transform: translate3d(-6px, -6px, 0);
   transition: transform 240ms cubic-bezier(0.175, 0.885, 0.32, 1.275) 220ms,
     background-color 240ms;
-  &.plain {
-    background-color: #fff;
-  }
 }
 </style>
