c0784b256be70bf82a8a42abca2933ff0ac77bd6
jocelyn <jocelyn@ecloudmobile.com>
2022-11-24 18:38:30 +0800

    feat: 新增監聽視窗關閉事件

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,30 @@
 
 <script>
 import TheHeader from '@/components/TheHeader.vue'
+
 export default {
   components: {
     TheHeader
+  },
+  methods: {
+    handleCloseWindow(e) {
+      const dialogText =
+        '還沒完成挑戰喔！確定要放棄嗎？(下次再挑戰要重頭開始喔！)'
+      e.returnValue = dialogText
+      return dialogText
+    }
+  },
+  mounted() {
+    window.addEventListener('beforeunload', (e) => this.handleCloseWindow(e))
+  },
+  unmounted() {
+    window.removeEventListener('beforeunload', (e) => this.handleCloseWindow(e))
   }
 }
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;500;700&display=swap');
 @import '@/assets/scss/variables';
 
 #app {
